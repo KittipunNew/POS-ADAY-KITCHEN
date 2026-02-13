5d477a83449489d2d92e5eca33565412c50e9afc
Kittipun <kittipun.dev@gmail.com>
2026-02-13 15:37:37 +0700

    style(navbar): remove fixed positioning

--- front-end/app/(customer)/tables/[tableId]/menu/_components/NavbarCustomer.tsx
+++ front-end/app/(customer)/tables/[tableId]/menu/_components/NavbarCustomer.tsx
@@ -3,7 +3,7 @@ import Logo from '../../../../../../components/navbar/Logo';
 
 const NavbarCustomer = () => {
   return (
-    <div className="flex items-center justify-between gap-5 bg-white p-5 shadow fixed top-0 w-full z-50">
+    <div className="flex items-center justify-between gap-5 bg-white p-5 border-b">
       <Logo />
       <DigitalClock />
     </div>
--- front-end/app/(customer)/tables/[tableId]/menu/layout.tsx
+++ front-end/app/(customer)/tables/[tableId]/menu/layout.tsx
@@ -8,7 +8,6 @@ export default function MenuLayout({
   return (
     <div className="flex flex-col xl:h-screen xl:overflow-hidden">
       <NavbarCustomer />
-      <div className="bg-[#f1f5f9] w-full h-20 shrink-0"></div>
       <div className="flex-1 overflow-hidden">{children}</div>
     </div>
   );
